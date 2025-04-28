9899103db7c28293c90795f8162eca1f6114702e
sketchugo <sketchugo@gmail.com>
2025-04-28 08:28:48 -0700

    commit test

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-const _ = require("lodash");
-
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-const user = "Abel Tesfaye";
-
-console.log(newItems);
-console.log(`Hello there ${user}`);
+setInterval(() => {
+    console.log("Hello");
+    
+}, 2000);
+console.log("I will log first");
