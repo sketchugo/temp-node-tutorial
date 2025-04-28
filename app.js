4d84856eac1266832e4ecfbb49bf85255ecbc45a
sketchugo <sketchugo@gmail.com>
2025-04-28 14:31:28 -0700

    new file created with writeFile

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-const { readFile } = require("fs");
+const { readFile, writeFile } = require("fs").promises;
+// const util = require("util");
 
-const getText = (path) => {
-    return new Promise((res, rej) => {
-        readFile(path, "utf8", (err, data) => {
-            if (err) {
-                rej(err);
-            } else {
-                res(data) 
-            };
-        });
-    });
-};
-
-// getText("./content/first.txt")
-//     .then(res => console.log(res))
-//     .catch(err => console.log(err));
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
 const start = async () => {
     try {
-        const first = await getText("./content/first.txt")
-        console.log(first);
-        
+        const first = await readFile("./content/first.txt", "utf8");
+        const second = await readFile("./content/second.txt", "utf8");
+        await writeFile("./content/result-mind-grenade.txt", `This is wack: ${first} // ${second}`, {flag: "a"});
+        console.log(first, second);        
     } catch (error) {
         console.log(error);
     };
 };
 
 start();
+
+// const getText = (path) => {
+//     return new Promise((res, rej) => {
+//         readFile(path, "utf8", (err, data) => {
+//             if (err) {
+//                 rej(err);
+//             } else {
+//                 res(data) 
+//             };
+//         });
+//     });
+// };
+
+// getText("./content/first.txt")
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err));
